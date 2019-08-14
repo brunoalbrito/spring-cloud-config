73dcfdbe48494da64bfe9b9c626b92054cbbd46b
Bruno Al. Brito <bruno.brito@startmeup.com.br>
2019-08-14 16:13:17 -0300

    Adicionando o básico para executar

---
+++ src/main/java/br/com/config/springcloudconfig/SpringCloudConfigApplication.java
@@ -0,0 +1,13 @@
+package br.com.config.springcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigApplication.class, args);
+	}
+
+}
